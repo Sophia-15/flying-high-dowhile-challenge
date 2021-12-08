598aede746dad6e378505516edd4a9ab209fbd2d
sophia-15 <deletrasopa@gmail.com>
2021-12-07 22:47:36 -0300

    :lipstick: adding theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Header />
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,11 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body { 
+    background-color: ${({theme}) => theme.colors.background};
   }
 
   @media (max-width: 1080px) {
