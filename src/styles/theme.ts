697306ff770eec0f221271dca842491152003171
sophia-15 <deletrasopa@gmail.com>
2021-12-07 22:15:22 -0300

    :zap: adding styles and ui things

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,10 @@ export default {
     background: '#09090B',
     backgroundLight: '#0E0E11',
 
+    descriptionText: '#E1E3EA',
+    footerText: '#A8A8B3',
+    white: '#FFFFFF',
+
     blue: '#2393FE',
     yellow: '#FFAC24',
     red: '#FF5757',
