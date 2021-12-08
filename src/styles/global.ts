3ec544a4984dd64091a21e0967a8c3e361ec2f53
sophia-15 <deletrasopa@gmail.com>
2021-12-07 22:30:48 -0300

    :lipstick: adding global style

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 760px) {
+    html {
+      font-size: 77.5%;
+    }
+  }
+
+  @media (max-width: 630px) {
+    html {
+      font-size: 67.5%;
+    }
+  }
+
+  @media (max-width: 550px) {
+    html {
+      font-size: 57.5%;
+    }
+  }
+`;
