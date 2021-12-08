7194b120e899b82846eac72572a290bdebada2a3
sophia-15 <deletrasopa@gmail.com>
2021-12-08 02:15:36 -0300

    :recycle: fixing height issues

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
-import { ContainerSponsors, ContentSponsors } from './styles'
+import { Container, ContainerSponsors, ContentSponsors } from './styles'
 
 export function Sponsors(){
   return (
-    <ContainerSponsors>
-      <h1>Patrocinadores</h1>
+    <Container>
+      <ContainerSponsors>
+        <h1>Patrocinadores</h1>
 
-      <h2>Empresas que acreditam no Projeto</h2>
-      <ContentSponsors>
-        <img src="/rocketseat.svg" alt="empresa parceira" />
+        <h2>Empresas que acreditam no Projeto</h2>
+        <ContentSponsors>
+          <img src="/rocketseat.svg" alt="empresa parceira" />
 
-        <img src="/rocketseat.svg" alt="empresa parceira" />
+          <img src="/rocketseat.svg" alt="empresa parceira" />
 
-        <img src="/rocketseat.svg" alt="empresa parceira" />
+          <img src="/rocketseat.svg" alt="empresa parceira" />
 
-        <img src="/rocketseat.svg" alt="empresa parceira" />
+          <img src="/rocketseat.svg" alt="empresa parceira" />
 
-        <img src="/rocketseat.svg" alt="empresa parceira" />
-      </ContentSponsors>
-    </ContainerSponsors>
+          <img src="/rocketseat.svg" alt="empresa parceira" />
+        </ContentSponsors>
+      </ContainerSponsors>
+    </Container>
   );
 }
